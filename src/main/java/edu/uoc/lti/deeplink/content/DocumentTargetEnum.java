fcfde09ecd52755b43821bf547f5145d856c4ac3
Xavi Aracil <xaracil@uoc.edu>
2019-11-26 11:08:55 +0100

    Added new  enum for possible values of document_target

---
+++ src/main/java/edu/uoc/lti/deeplink/content/DocumentTargetEnum.java
@@ -0,0 +1,13 @@
+package edu.uoc.lti.deeplink.content;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * @author Xavi Aracil <xaracil@uoc.edu>
+ */
+@AllArgsConstructor
+public enum DocumentTargetEnum {
+  frame,
+  iframe,
+  window;
+}
