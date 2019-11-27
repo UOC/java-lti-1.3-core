83867d38bcc6591405d97a379c9deb9286b29f7a
Xavi Aracil <xaracil@uoc.edu>
2019-11-27 14:21:34 +0100

    AccessTokenRequestBuilder and some implementations

--- src/main/java/edu/uoc/lti/accesstoken/AccessTokenRequestBuilder.java
+++ src/main/java/edu/uoc/lti/accesstoken/AccessTokenRequestBuilder.java
@@ -1,12 +1,14 @@
 package edu.uoc.lti.accesstoken;
 
+import java.io.IOException;
+
 public interface AccessTokenRequestBuilder {
   /**
    * Returns the request for obtaing the access token
    * @param request request to build
    * @return string with the request to pass to the authorization server
    */
-  String build(AccessTokenRequest request);
+  String build(AccessTokenRequest request) throws IOException;
 
   /**
    * Gets the content type of the request to the authorization server
