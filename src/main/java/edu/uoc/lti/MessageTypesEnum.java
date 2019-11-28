f2e9e096c6628926df1ed43acfbb4f2811bd7a7f
Xavi Aracil <xaracil@uoc.edu>
2019-11-28 11:35:03 +0100

    Removed constructor

--- src/main/java/edu/uoc/lti/MessageTypesEnum.java
+++ src/main/java/edu/uoc/lti/MessageTypesEnum.java
@@ -1,11 +1,8 @@
 package edu.uoc.lti;
 
-import lombok.AllArgsConstructor;
-
 /**
  * @author Xavi Aracil <xaracil@uoc.edu>
  */
-@AllArgsConstructor
 public enum  MessageTypesEnum {
 	LtiResourceLinkRequest,
 	LtiDeepLinkingRequest
