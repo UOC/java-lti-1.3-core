8aa9584c8a0acb359168f7528585380d12f16ecd
Xavi Aracil <xaracil@uoc.edu>
2019-12-04 21:17:07 +0100

    Added missing @throws documentation

--- src/main/java/edu/uoc/lti/accesstoken/AccessTokenRequestBuilder.java
+++ src/main/java/edu/uoc/lti/accesstoken/AccessTokenRequestBuilder.java
@@ -7,6 +7,7 @@ public interface AccessTokenRequestBuilder {
    * Returns the request for obtaing the access token
    * @param request request to build
    * @return string with the request to pass to the authorization server
+   * @throws IOException if something went wrong getting the access token
    */
   String build(AccessTokenRequest request) throws IOException;
 
